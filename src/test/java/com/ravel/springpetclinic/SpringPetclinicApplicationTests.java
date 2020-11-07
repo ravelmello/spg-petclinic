6eb3f380cb75d11ea3e3128bd1e18e10120414d2
Ravel da Costa Melo <ravelmello3@gmail.com>
2020-11-07 11:00:51 -0300

    Initial project add

---
+++ src/test/java/com/ravel/springpetclinic/SpringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravel.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
