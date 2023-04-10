e20864dc49fec6d369bda00789c6ace234b26974
ravelmello <ravelmello3@gmail.com>
2023-04-10 11:05:20 -0300

    Alter the repo insert form and list all vets . fixes #16

--- pet-clinic-web/src/main/java/com/ravel/petclinic/controllers/VeterinarianController.java
+++ pet-clinic-web/src/main/java/com/ravel/petclinic/controllers/VeterinarianController.java
@@ -1,14 +1,26 @@
 package com.ravel.petclinic.controllers;
 
 
+import com.ravel.petclinic.service.VeterinarianService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VeterinarianController {
 
+    public VeterinarianController(VeterinarianService veterinarianService) {
+        this.veterinarianService = veterinarianService;
+    }
+
+    private final VeterinarianService veterinarianService;
+
+
     @RequestMapping({"/vets", "/vets/index" ,"/vets/index.html"})
-    public String vetIndex() {
+
+    public String vetIndex(Model model) {
+        model.addAttribute("vets", veterinarianService.getAllVets());
+
         return "vets/index";
     }
 }
